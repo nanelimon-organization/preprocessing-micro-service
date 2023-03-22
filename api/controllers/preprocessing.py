c487bbe3c155e4f21577aa9a0417cfae32b0de98
Seyma SARIGİL <seymasargil@gmail.com>
2023-03-22 22:04:50 +0300

    endpoint parameters added

--- api/controllers/preprocessing.py
+++ api/controllers/preprocessing.py
@@ -21,18 +21,23 @@ class Item(BaseModel):
             raise ValueError('Text cannot be empty or contain only whitespace')
         return v
 
+
 @preprocessing_router.post("/", response_model=dict)
-async def preprocessor(item: Item):
+async def preprocessor(item: Item, turkish_char: bool):
     """
     Preprocess text using Mintlemon Turkish NLP library.
 
     This API takes in a text and applies various preprocessing steps including offensive contraction replacement,
     Turkish character normalization, numeric text normalization, lowercase conversion, and short text removal.
 
+
     Parameters
     ----------
     item : Item
         The input text to be preprocessed.
+    turkish_char : bool
+        "POST /preprocessing/?turkish_char=true HTTP/1.1" 200 OK
+        "POST /preprocessing/?turkish_char=false HTTP/1.1" 200 OK
 
     Returns
     -------
@@ -46,8 +51,8 @@ async def preprocessor(item: Item):
 
     """
     try:
-        preprocessor = DataPreprocessor(item.text, supported_turkish_chars=True)
+        preprocessor = DataPreprocessor(item.text, supported_turkish_chars=turkish_char)
         result = preprocessor.preprocess()
         return {"result": result}
-    except:
-        raise HTTPException(status_code=400, detail='Error processing text')
+    except Exception as ex:
+        raise HTTPException(status_code=400, detail=f'Error processing text, {ex}')
