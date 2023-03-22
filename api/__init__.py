a4d137e4b57485d1ece742e559565cbb3b4fa784
Seyma SARIGİL <seymasargil@gmail.com>
2023-03-22 20:34:34 +0300

    fix/include_router description error

--- api/__init__.py
+++ api/__init__.py
@@ -1,15 +1,11 @@
 from fastapi import APIRouter
 from .controllers.preprocessing import preprocessing_router
 
-router = APIRouter(
-    prefix="/api",
-    tags=["API"],
-    description="API Endpoints for Preprocessing Micro Service",
-)
+router = APIRouter()
+
 
 router.include_router(
     preprocessing_router,
     prefix="/preprocessing",
-    tags=["Preprocessing"],
-    description="API endpoints for text preprocessing using Mintlemon Turkish NLP library",
+    tags=["Preprocessing"]
 )
